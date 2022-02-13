488cfe345aece00b390bf2e3f53d8c75fe281af7
aditya <vinchuaditya@gmail.com>
2022-02-13 17:47:47 +0530

    add node at position

--- LinkedList/LinkedList.cs
+++ LinkedList/LinkedList.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace LinkedList
 {
     public class LinkedList
-    {
+    {   
         public Node head;
 
         public void InsertFirst(int data)
@@ -44,6 +44,46 @@ namespace LinkedList
             }
         }
 
+
+        public void InsertAtPos(int data ,int pos)
+        {
+            Node newnode = new Node(data);
+            if ((pos<1) || pos>Count())
+            {
+                Console.WriteLine("Enter a valid position");
+            }
+            if(pos==1)
+            {
+                InsertFirst(data);
+            }
+            if(pos>Count())
+            {
+                InsertLast(data);
+            }
+            else
+            {
+                
+                Node temp = head;
+                for (int i = 1; i < pos - 1; i++)
+                {
+                    temp = temp.next; 
+                }
+                newnode.next = temp.next;
+                temp.next = newnode;
+            }
+        }
+
+        public int Count()
+        {
+            Node temp = head;
+            int count = 0;
+            while (temp != null)
+            {
+                count++;
+                temp = temp.next;
+            }
+            return count;
+        }
 
         public void DisplayAll()
         {
--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -14,11 +14,13 @@ namespace LinkedList
 
 
             linkedlist.InsertLast(56);
-            linkedlist.InsertLast(30);
+            
             linkedlist.InsertLast(70);
-
+            linkedlist.InsertAtPos(30, 2);
             linkedlist.DisplayAll();
+            
             Console.WriteLine();
+            Console.WriteLine("Total Nodes:"+ linkedlist.Count());
             Console.ReadLine();
 
 
