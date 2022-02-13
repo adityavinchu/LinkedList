6765b319527e576e6c2b6931a53199ef5d6cd5d1
aditya <vinchuaditya@gmail.com>
2022-02-13 16:17:20 +0530

    initial

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class LinkedList
+    {
+        public Node head;
+
+        public void InsertFirst(int data)
+        {
+            Node newNode = new Node(data);
+            if(head == null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                newNode.next = head;
+                head = newNode;
+            }
+        }
+
+
+        public void DisplayAll()
+        {
+            Node temp = head;
+            if (temp == null)
+            {
+                Console.WriteLine("List is empty");
+            }
+            else
+            {
+                while (temp != null)
+                {
+                    temp = temp.next;
+                    Console.Write(temp.data + " ");
+                    Console.Write("->");
+                }
+            }
+            Console.Write("NULL");
+        }
+
+    }
+
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList linkedlist = new LinkedList();
+            linkedlist.DisplayAll();
+            Console.WriteLine();
+
+            linkedlist.InsertFirst(56);
+            linkedlist.InsertFirst(30);
+            linkedlist.InsertFirst(70);
+            Console.ReadLine();
+
+        }
+    }
+}
